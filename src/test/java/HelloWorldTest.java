fdc72ccd4bcd3d98087c29519475284ae9b0bb46
Max Kolodkin <MKolodkin@alfabank.ru>
2025-06-15 16:28:15 +0500

    Ex10: Тест на короткую фразу Ex11: Тест запроса на метод cookie Ex12: Тест запроса на метод header Ex13: User Agent Ex14: Формирование фреймворка

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -4,11 +4,16 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HelloWorldTest {
 
@@ -193,5 +198,66 @@ public class HelloWorldTest {
                 .andReturn();
 
         responseForCheck.print();
+    }
+
+    @Test
+    public void testFor200() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/map")
+                .andReturn();
+//        assertTrue(response.statusCode() == 200,"Unexpected status code");
+        assertEquals(200, response.statusCode(),"Unexpected status code");
+    }
+
+    @Test
+    public void testFor404() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/map2")
+                .andReturn();
+//        assertTrue(response.statusCode() == 200,"Unexpected status code");
+        assertEquals(404, response.statusCode(),"Unexpected status code");
+    }
+
+    @Test
+    public void testHelloMethodWithoutName() {
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+        String answer = response.getString("answer");
+        assertEquals("Hello, someone", answer, "The answer is not expected");
+
+    }
+
+    @Test
+    public void testHelloMethodWithName() {
+        String name = "Username";
+        JsonPath response = RestAssured
+                .given()
+                .queryParam("name", name)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+        String answer = response.getString("answer");
+        assertEquals("Hello, " + name, answer, "The answer is not expected");
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "John", "Pete"})
+    public void testHelloMethodWithNameParameterized(String name) {
+        Map<String, String> queryParams = new HashMap<>();
+
+        if (name.length() > 0) {
+            queryParams.put("name", name);
+        }
+
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(queryParams)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+        String answer = response.getString("answer");
+        String expectedName = (name.length() > 0) ? name : "someone";
+        assertEquals("Hello, " + expectedName, answer, "The answer is not expected");
+
     }
 }
--- src/test/java/HomeworkTests.java
+++ src/test/java/HomeworkTests.java
@@ -1,11 +1,19 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
 import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.path.xml.XmlPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HomeworkTests {
 
@@ -130,5 +138,54 @@ public class HomeworkTests {
             }
 
         }
+    }
+
+    @Test
+    public void testEx10() {
+        String hello = "Hello, world";
+        assertTrue(hello.length() > 15, "В строке меньше 15 символов!");
+    }
+
+    @Test
+    public void testEx11() {
+        Response responseCheckCookies = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+        Map<String, String> cookies = responseCheckCookies.getCookies();
+        assertEquals("hw_value", cookies.get("HomeWork"), "Значение куки из ответа не соответствует HomeWork -> hw_value");
+    }
+
+    @Test
+    public void testEx12() {
+        Response responseCheckHeader = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_header")
+                .andReturn();
+        Headers headers = responseCheckHeader.getHeaders();
+        assertEquals(
+                "Some secret value",
+                headers.getValue("x-secret-homework-header"),
+                "Значение заголовка 'x-secret-homework-header' из ответа не соответствует значению 'Some secret value' или такой заголовок отсутствует ");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30', Mobile, No, Android",
+            "'Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1', Mobile, Chrome, iOS",
+            "'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)', Googlebot, Unknown, Unknown",
+            "'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0', Web, Chrome, No",
+            "'Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1', Mobile, No, iPhone"
+    })
+    public void testEx13(String userAgent, String expectedPlatform, String expectedBrowser, String expectedDevice) {
+        Response response = RestAssured
+                .given()
+                .header("User-Agent", userAgent)
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .andReturn();
+
+        response.prettyPrint();
+        assertEquals(expectedPlatform, response.jsonPath().getString("platform"));
+        assertEquals(expectedBrowser, response.jsonPath().getString("browser"));
+        assertEquals(expectedDevice, response.jsonPath().getString("device"));
+
     }
 }
